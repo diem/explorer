8fa1c32cd8e1d414463a03e0f04102e26687783d
Ankush Agarwal <ankushagarwal11@gmail.com>
2021-10-26 12:07:45 -0700

    Add tf module for deployment

---
+++ terraform/modules/explorer/main.tf
@@ -0,0 +1,23 @@
+resource "helm_release" "explorer" {
+  name  = "explorer"
+  chart = "${path.module}/../../../helm"
+  wait  = false
+  set {
+    name  = "timestamp"
+    value = timestamp()
+  }
+  values = [
+    jsonencode({
+      repo                   = var.image_repo
+      tag                    = var.image_tag
+      hostname               = var.hostname
+      ingressAnnotations     = var.ingress_annotations
+      chain                  = var.chain
+      env                    = var.env
+      base_url               = var.base_url
+      graphql_url            = var.graphql_url
+      blockchain_jsonrpc_url = var.blockchain_jsonrpc_url
+      blockchain_restapi_url = var.blockchain_restapi_url
+    }),
+  ]
+}
